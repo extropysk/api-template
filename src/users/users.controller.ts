e6f278ce9560fbfef3033f8a04fe3efd729c2154
Kristián Duda <kristian.duda@extropy.sk>
2023-07-05 05:45:59 +0200

    remove user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common'
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { Current } from 'src/core/decorators/current.decorator'
 import { Jwt } from 'src/core/decorators/jwt.decorator'
+import { PayloadDto } from 'src/core/dto/payload.dto'
 import { Payload } from 'src/core/interfaces/payload.interface'
-import { UserDto } from 'src/users/dto/user.dto'
 import { UsersService } from 'src/users/users.service'
 
 @ApiTags('users')
@@ -13,7 +13,7 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @ApiOperation({ summary: 'Get current user' })
-  @ApiOkResponse({ type: UserDto })
+  @ApiOkResponse({ type: PayloadDto })
   @Get('current')
   getProfile(@Current() current: Payload) {
     return this.usersService.getCurrentUser(current)
